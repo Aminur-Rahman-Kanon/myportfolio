c6961dbac32eee31db12dd5a5d1bbf9313db938f
Aminur-Rahman-Kanon <kanon754@gmail.com>
2023-07-21 13:51:36 +0100

    final commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,8 @@ function App() {
                 behavior: 'smooth',
                 block: 'start',
                 inline: 'center'
-            })
+            });
+            setItem(id);
         }
         else {
             setItem(id);
