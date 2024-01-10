f7c26dec79aab2fe1a19985ad277e1c4b2e40214
Aminur-Rahman-Kanon <kanon754@gmail.com>
2024-01-10 00:24:11 +0000

    update projects

--- src/Components/pages/Projects/projects.jsx
+++ src/Components/pages/Projects/projects.jsx
@@ -4,6 +4,7 @@ import cycle from '../../../Assets/projects/cycleFix.png';
 import karkhana from '../../../Assets/projects/karkhana.png';
 import phone from '../../../Assets/projects/phoneClinic.png';
 import nihon from '../../../Assets/projects/nihonChukoSha.png';
+import boxdelabonita from '../../../Assets/projects/box.png';
 
 function Projects () {
     return (
@@ -11,20 +12,20 @@ function Projects () {
             <h3 className={styles.projectsHeaderH3}>Recent Works</h3>
             <hr className={styles.hr}/>
             <div className={styles.projectsItemContainer}>
-                <a href='https://nihonchukosha.onrender.com' target='_blank' className={styles.projectsItem}>
+                <a href='https://boxdelabonita.com' target='_blank' className={styles.projectsItem}>
                     <div className={styles.projectsImgContainer}>
-                        <img src={nihon} className={styles.projectsImg}/>
+                        <img src={boxdelabonita} className={styles.projectsImg}/>
                     </div>
                     <div className={styles.projectsDetailsContainer}>
-                        <h3 className={styles.projectsH3}>Semi commerce website for cars auction and parts</h3>
+                        <h3 className={styles.projectsH3}>An online store for selling womens bag and purse</h3>
                     </div>
                 </a>
-                <a href='https://phoneclinic.onrender.com' target='_blank' className={styles.projectsItem}>
+                <a href='https://karkhana.onrender.com' target='_blank' className={styles.projectsItem}>
                     <div className={styles.projectsImgContainer}>
-                        <img src={phone} className={styles.projectsImg}/>
+                        <img src={karkhana} className={styles.projectsImg}/>
                     </div>
                     <div className={styles.projectsDetailsContainer}>
-                        <h3 className={styles.projectsH3}>An online phone tablet and laptop repair booking service</h3>
+                        <h3 className={styles.projectsH3}>An online store for selling womens casual jewelleries</h3>
                     </div>
                 </a>
                 <a href='https://cyclefix.onrender.com' target='_blank' className={styles.projectsItem}>
@@ -35,12 +36,20 @@ function Projects () {
                         <h3 className={styles.projectsH3}>An online bike and e-scooter repair booking service</h3>
                     </div>
                 </a>
-                <a href='https://karkhana.onrender.com' target='_blank' className={styles.projectsItem}>
+                <a href='https://phoneclinic.onrender.com' target='_blank' className={styles.projectsItem}>
                     <div className={styles.projectsImgContainer}>
-                        <img src={karkhana} className={styles.projectsImg}/>
+                        <img src={phone} className={styles.projectsImg}/>
                     </div>
                     <div className={styles.projectsDetailsContainer}>
-                        <h3 className={styles.projectsH3}>An online store for selling womens casual jewelleries</h3>
+                        <h3 className={styles.projectsH3}>An online phone tablet and laptop repair booking service</h3>
+                    </div>
+                </a>
+                <a href='https://nihonchukosha.onrender.com' target='_blank' className={styles.projectsItem}>
+                    <div className={styles.projectsImgContainer}>
+                        <img src={nihon} className={styles.projectsImg}/>
+                    </div>
+                    <div className={styles.projectsDetailsContainer}>
+                        <h3 className={styles.projectsH3}>Semi commerce website for cars auction and parts</h3>
                     </div>
                 </a>
             </div>
